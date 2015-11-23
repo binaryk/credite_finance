0c9428b3b302921824e6db7398a230e2f3828b30
binaryk <lupacescueduard@yahoo.com>
2015-11-23 13:35:50 +0200

    Curent work

--- app/controllers/extern/PrepareControlsExternController.php
+++ app/controllers/extern/PrepareControlsExternController.php
@@ -1,11 +1,12 @@
 <?php
 namespace Credite\Clienti;
 use Credite\PersoaneFizice;
+use Credite\PersoaneFizice\Form\PersoaneFiziceForm;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Credite\PersoaneFizice\Form\PrimaCasaForm;
 
-class PrepareControlsExternController  extends PrimaCasaForm{
+class PrepareControlsExternController  extends PersoaneFiziceForm{
     protected $layout = 'template.layout';
 
 
