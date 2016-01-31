8105d9f48eef469e9517ad808d8373de5870985a
binaryk <lupacescueduard@yahoo.com>
2016-01-31 02:06:34 +0200

    Denumirea

--- app/views/template/parts/body/~user-login.blade.php
+++ app/views/template/parts/body/~user-login.blade.php
@@ -1,7 +1,8 @@
 @if(! Request::is('confirm-link/*'))
 <li class="dropdown dropdown-user">
 	<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-	{{HTML::image("assets/admin/layout/img/logo_creditfin.png",null, ['width' => '40', 'height' => '15']) }}<span class="username username-hide-on-mobile"> KesInvest </span>
+	{{HTML::image("assets/admin/layout/img/logo_creditfin.png",null, ['width' => '40', 'height' => '15']) }}
+		<span class="username username-hide-on-mobile"> FinanceFlow </span>
 	<i class="fa fa-angle-down"></i></a>
 	<ul class="dropdown-menu dropdown-menu-default"> 
 		<li><a href="{{ URL::route('user-profile') }}"><i class="icon-user"></i> Profil utilizator </a></li>
